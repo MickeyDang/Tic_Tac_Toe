1a0e63a7c88928f58921871083823b97940d798b
Mickey Dang <mickeydang7299@gmail.com>
2017-09-27 22:32:59 -0400

    Commented Code
    Commented code for clarity and added .class file

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
 import java.lang.Math;
 public class TicTacToe {
+	//game board
 	static int[][] mGrid = new int[3][3];
 	
 	public static void main(String[] args) {
+		//initializes the turn of player one
 		int turn = 1;
 		System.out.println("The grid is set up as \n 0 1 2 \n 3 4 5 \n 6 7 8" + "");
 		displayGrid();
 		
+		//runs as long as game is not won
 		while (gameWon(1) == false || gameWon(2) == false) {
 			
+			//player one turn
 			if (turn == 1) {
 				makeMove(1);
 				turn = 2;
@@ -20,6 +24,7 @@ public class TicTacToe {
 				}
 			}
 			
+			//player two turn
 			if (turn == 2) {
 				makeMove(2);
 				turn = 1;
@@ -33,25 +38,31 @@ public class TicTacToe {
 		}	
 	}
 	
+	//method to handle player making move (typing in number)
 	private static void makeMove(int a) {
 		System.out.println("Player " + a + "'s turn! Type in a number from 0 to 8");
 		Scanner scanner = new Scanner(System.in);
 		int x = scanner.nextInt();
+		//handles input outside of range
 		while (x > 8 || x < 0) {
 			System.out.println("That is an invalid input try again");
 			x = scanner.nextInt();
 		}		
+		//handles input of an already occupied square
 		while (mGrid[x/3][x%3] != 0) {
 			System.out.println("That spot is taken please try again!");
-			x = scanner.nextInt();
+			x = scanner.nextInt();	
 		} 
-		
+		//assigns a value to square on grid
+		//0 is default (no one), 1 is player 1, 2 is player 2
+		//calculations done to turn input of 0-8 into the corresponding (y,x) coordinate
 		mGrid[x/3][x%3] = a;
-		
 	}
 	
+	//displays the grid
 	private static void displayGrid() {
 		System.out.println("");
+		//iterates through the grid and displays X (player 1) or O (player 2) or [] if unoccupied
 		for (int x = 0; x < mGrid.length ; x++) {
 			for (int y = 0 ; y < mGrid.length ; y++) {
 				if (mGrid[x][y] == 0) {
@@ -66,12 +77,19 @@ public class TicTacToe {
 		}
 	}
 	
+	//checks if game is won
+	//number is the code corresponding to a player occupied square (1 or 2)
 	private static boolean gameWon(int number) {
-		boolean rowOfThree = true;
+		//starts as true. if the loop catches a non player number it switches to false.
 		boolean diagonalOfThree = true;
 		boolean altDiagonalOfThree = true;
+		
+		//iterates through the grid column by column left to right
 		for (int x = 0; x < mGrid.length ; x++) {
+			//reinitializes the variable at the start of each loop when it checks a different column and row
 			boolean columnOfThree = true;
+			boolean rowOfThree = true;
+	
 			if (mGrid[x][x] != number) {
 				diagonalOfThree = false;
 			}	
@@ -79,10 +97,10 @@ public class TicTacToe {
 				altDiagonalOfThree = false;
 			}
 			for (int y = 0; y < mGrid.length; y++) {
-				if (mGrid[x][y] != number) {
+				if (mGrid[y][x] != number) {
 					columnOfThree = false;
 				}
-				if (mGrid[y][x] != number) {
+				if (mGrid[x][y] != number) {
 					rowOfThree = false;
 				}
 			}
@@ -92,16 +110,19 @@ public class TicTacToe {
 			if (rowOfThree) {
 				return true;
 			}
-		}	
+		}
 		if (diagonalOfThree) {
 			return true;
 		}	
 		if (altDiagonalOfThree) {
 			return true;
 		}
+		
+		//default return
 		return false;
 	}
 	
+	//congratulates winner
 	private static void congratWinner(int n) {
 		System.out.println("Congrats player" + n + " you won!");
 	}
